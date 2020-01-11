2ff6587e1d47a42ab13fc9abfe0e9924fbb69bde
Nagarasa Miththilan <42810822+Miththilan@users.noreply.github.com>
2020-01-11 14:34:39 +0530

    ocruser Service

--- src/main/java/com/example/ocr/Service/OcrUserService.java
+++ src/main/java/com/example/ocr/Service/OcrUserService.java
@@ -1,9 +1,21 @@
 package com.example.ocr.Service;
+import com.example.ocr.Model.OcrUser;
+import com.example.ocr.Repository.OcrUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OcrUserService {
+    @Autowired
+    OcrUserRepository ocrUserRepository;
 
+    public OcrUserService(OcrUserRepository ocrUserRepository) {
+        this.ocrUserRepository=ocrUserRepository;
+    }
+
+    public List<OcrUser>getAll(){
+        return (List<OcrUser>) ocrUserRepository.findAll();
+    }
 }
