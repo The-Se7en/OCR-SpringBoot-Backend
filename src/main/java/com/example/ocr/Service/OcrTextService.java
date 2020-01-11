bd4304acc1ea573e791ba1063ae7a31597160eb6
Nagarasa Miththilan <42810822+Miththilan@users.noreply.github.com>
2020-01-11 14:30:51 +0530

    ocrTextservice

--- src/main/java/com/example/ocr/Service/OcrTextService.java
+++ src/main/java/com/example/ocr/Service/OcrTextService.java
@@ -1,7 +1,14 @@
 package com.example.ocr.Service;
+import com.example.ocr.Repository.OcrTextMobileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OcrTextService {
+    @Autowired
+    OcrTextMobileRepository ocrTextMobileRepository;
 
+    public OcrTextService(OcrTextMobileRepository ocrTextMobileRepository) {
+        this.ocrTextMobileRepository = ocrTextMobileRepository;
+    }
 }
